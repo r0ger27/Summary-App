222c1f672c3630989b7d721031bf0cc28143fc4d
r0ger27 <rsafin90@gmail.com>
2019-04-25 10:35:37 +0300

    Initial Commit

---
+++ Summary App/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Summary App
+//
+//  Created by Руслан Сафин on 25/04/2019.
+//  Copyright © 2019 Ruslan Safin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
