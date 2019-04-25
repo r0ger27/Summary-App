63de34d2cb261623ccc817c5759426bf806dbb93
r0ger27 <rsafin90@gmail.com>
2019-04-25 11:38:39 +0300

    Final Version 1.0.0

--- Summary App/ViewController.swift
+++ Summary App/ViewController.swift
@@ -2,17 +2,12 @@
 //  ViewController.swift
 //  Summary App
 //
-//  Created by Руслан Сафин on 25/04/2019.
+//  Created by Ruslan Safin on 25/04/2019.
 //  Copyright © 2019 Ruslan Safin. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
 }
 
